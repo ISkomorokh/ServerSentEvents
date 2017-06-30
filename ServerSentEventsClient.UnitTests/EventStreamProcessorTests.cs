796b1ce625adab541415c2e272838dfed42127cf
Igor Skomorokh <igor.skomorokh@d2l.com>
2017-06-30 14:25:29 +0300

    Added some tests

---
+++ ServerSentEventsClient.UnitTests/EventStreamProcessorTests.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using ServerSentEventsClient.Default;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerSentEventsClient.UnitTests {
+
+	[TestFixture]
+	internal sealed class EventStreamProcessorTests {
+
+		private IEventStreamProcessor m_sut;
+
+		private static Stream GetStream( string source ) {
+			return new MemoryStream( Encoding.UTF8.GetBytes( source ) );
+		}
+
+		[SetUp]
+		public void Setup() {
+			m_sut = new EventStreamProcessor( new ServerSentEventsMessageParser() );
+		}
+
+		[Test]
+		public void ProcessAsync_NoOnMessageHandler_ShoudntThrow() {
+			Assert.DoesNotThrowAsync( async () => await m_sut.ProcessAsync( new MemoryStream(), CancellationToken.None ) );
+		}
+
+		[Test]
+		public async Task ProcessAsync_OnMessageHandler_ExecutedOnce() {
+			int executionCount = 0;
+			IServerSentEventsMessage actrualMessage = null;
+
+			m_sut.OnMessage = message => {
+				executionCount++;
+				actrualMessage = message;
+			};
+
+			string data = "data: Hello, World!\r\n\r\n";
+
+			using ( var stream = GetStream( data ) ) {
+				await m_sut.ProcessAsync( stream, CancellationToken.None );
+			}
+
+			Assert.That( executionCount, Is.EqualTo( 1 ) );
+			actrualMessage.ShouldBeEquivalentTo( new ServerSentEventsMessage { Data = "Hello, World!" } );
+		}
+
+		[Test]
+		public async Task ProcessAsync_OnMessageHandler_ExecutedTwice() {
+			var actual = new List<IServerSentEventsMessage>();
+
+			m_sut.OnMessage = message => {
+				actual.Add( message );
+			};
+
+			string data = "data: Hello, World!\r\n\r\ndata: Good bye!\r\n\r\n";
+
+			using ( var stream = GetStream( data ) ) {
+				await m_sut.ProcessAsync( stream, CancellationToken.None );
+			}
+
+			var expected = new List<IServerSentEventsMessage> {
+				new ServerSentEventsMessage { Data = "Hello, World!" },
+				new ServerSentEventsMessage { Data = "Good bye!" }
+			};
+			actual.ShouldBeEquivalentTo( expected, config => config.WithStrictOrdering() );
+		}
+
+	}
+
+}
--- ServerSentEventsClient.UnitTests/ServerSentEventsClientTests.cs
+++ ServerSentEventsClient.UnitTests/ServerSentEventsClientTests.cs
@@ -1,16 +1,71 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
 
 namespace ServerSentEventsClient.UnitTests {
 
 	[TestFixture]
 	internal sealed class ServerSentEventsClientTests {
 
-		private IServerSentEventsClient m_sut = new ServerSentEventsClient( "https://stream.launchdarkly.com/", "flags" );
+		private IServerSentEventsClient m_sut;
+		private Mock<IEventStreamClient> m_mockedEventStreamClient;
+		private Mock<IEventStreamProcessor> m_mockedEventStreamProcessor;
+		private MockRepository m_mockRepository;
+
+		[SetUp]
+		public void SetUp() {
+			m_mockRepository = new MockRepository( MockBehavior.Strict );
+			m_mockedEventStreamClient = m_mockRepository.Create<IEventStreamClient>();
+			m_mockedEventStreamProcessor = m_mockRepository.Create<IEventStreamProcessor>();
+
+			m_sut = new ServerSentEventsClient(
+				m_mockedEventStreamClient.Object,
+				m_mockedEventStreamProcessor.Object
+			);
+		}
 
 		[Test]
-		public void Test() {
+		public void Start_WhenEventStreamClientFailedToStart_Throws() {
+			m_mockedEventStreamClient.Setup( c => c.StartAsync() ).Throws<Exception>();
 
+			Assert.Throws<Exception>( () => m_sut.Start() );
+			m_mockRepository.VerifyAll();
 		}
+
+		[Test]
+		public void Start_WhenEventStreamClientStartedSuccessfully_ReturnsSelf() {
+			var stream = new MemoryStream();
+
+			m_mockedEventStreamClient.Setup( c => c.StartAsync() ).ReturnsAsync( stream );
+			m_mockedEventStreamProcessor.Setup( p => p.ProcessAsync( stream, It.IsAny<CancellationToken>() ) ).Returns( Task.CompletedTask );
+
+			IServerSentEventsClient result = m_sut.Start();
+
+			Assert.That( result, Is.EqualTo( m_sut ) );
+			m_mockRepository.VerifyAll();
+		}
+
+		[Test]
+		public void Dispose_CallsEventStreamClientDispose() {
+			m_mockedEventStreamClient.Setup( c => c.Dispose() );
+
+			m_sut.Dispose();
+
+			m_mockRepository.VerifyAll();
+		}
+
+		[Test]
+		public void Stop_CallsEventStreamClientDispose() {
+			m_mockedEventStreamClient.Setup( c => c.Dispose() );
+
+			m_sut.Stop();
+
+			m_mockRepository.VerifyAll();
+		}
+
 	}
 
 }
